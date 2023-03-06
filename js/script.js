f88a91356126bf36e28d3e0dfa612f459afe8f62
achica10 <achica@hawk.iit.edu>
2023-03-06 02:42:17 -0600

    Added a new js function and styled it

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+function info() {
+   alert("This raffle contains a total of 1000 participants. Spots fill up on a first come first served basis. The winner will be contacted via email and via a phone text message.");
+}
+
+/* Calling function */
+document.getElementById("theButton").onclick=info;
